f74d5be843820905e0d66bf6099f3b6951ae5848
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-08-16 22:55:06 +0300

    commit tree 7da303863cb1309b830f1d83a8ba6fe1a1644420

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+# Write a method #oxford_comma in the lib/oxford_comma.rb file that takes an array of string elements as an argument and converts it into a string using the Oxford comma (Links to an external site.).
 
+# oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+# # => "fiddleheads, okra, and kohlrabi"
+# Hint: You will need to refer to the section above about converting arrays into strings, but note that coding this method will involve a couple of extra challenges.
+
+# Hint: Remember, strings can be operated on very similarly to arrays. For instance, you can add elements to the end of strings with the << ("shovel") method just like you can with arrays.
+
+def oxford_comma(array)
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array[0] + " and " + array[1]
+  else
+    array[-1] = "and " + array[-1]
+    return array.join(", ")
+  end
 end
